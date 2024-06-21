ed24d84caf9056c0a8302d2945b9b1d511145bf5
MemechiKekamoto <memechi.kekamoto@pm.me>
2024-06-21 15:43:28 +0900

    added config files

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import baseConfig from '@polkadot/dev/config/eslint';
+
+export default [
+  ...baseConfig,
+  {
+    rules: {
+      // add override for any (a metric ton of them, initial conversion)
+      '@typescript-eslint/no-explicit-any': 'off',
+      // we generally use this in isFunction, not via calling
+      '@typescript-eslint/unbound-method': 'off'
+    }
+  }
+];
